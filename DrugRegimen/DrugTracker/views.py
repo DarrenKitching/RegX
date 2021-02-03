2a20b8d20874e48c476b9f3a786d6627861f8af3
Darren Kitching <39224893+DarrenKitching@users.noreply.github.com>
2021-02-03 12:27:46 +0000

    Login System

---
+++ DrugRegimen/DrugTracker/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'login.html')
+
+def home(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
