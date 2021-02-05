20fb2f7273e2087491c7eaea793fc2b7a0f9ddcd
ConOLeary <conolaoghaire@gmail.com>
2021-02-05 17:21:59 +0000

    Replaced hard-coded integer value definitions with constants

--- DrugRegimen/DrugTracker/models.py
+++ DrugRegimen/DrugTracker/models.py
@@ -1,16 +1,18 @@
 from django.db import models
+import consts as c
 
 # Create your models here.
 class Perscription(models.Model):
-	patient = models.CharField(max_length=50) # username of patient
-	doctor = models.CharField(max_length=50) # username of doctor
-	medicationCode = models.CharField(max_length=50) # code to identify the medication
-	medicationDose = models.CharField(max_length=50) # dosage to be taken
+	patient = models.CharField(max_length=c.MAX_NAME_LEN) # username of patient
+	doctor = models.CharField(max_length=c.MAX_NAME_LEN) # username of doctor
+	medicationCode = models.CharField(c.MAX_MEDCODE_LEN) # code to identify the medication
+	medicationDose = models.IntegerField() # dosage to be taken
+	doseUnit = models.CharField(c.MAX_UNIT_LEN) # unit of dosage
 	timesPerDay = models.IntegerField()
 	intervalBetweenDoses = models.IntegerField()
 	perscriptionStartDate = models.DateField()
 	totalNumberOfDoses = models.IntegerField()
 
 class Medication(models.Model):
-	medicationCode = models.CharField(max_length=50) # key to identify medication
-	medicationName = models.CharField(max_length=50) # name of the medication
+	medicationCode = models.CharField(c.MAX_MEDCODE_LEN) # key to identify medication
+	medicationName = models.CharField(c.MAX_NAME_LEN) # name of the medication
